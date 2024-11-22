d1144e09762275ce78e150adad319f40345d5549
SUDHAKAR1202 <sudhakargmk12@gmail.com>
2024-11-22 17:07:05 +0530

    file-upload-clientside

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [file, setFile] = useState(null);
+  const [metadata, setMetadata] = useState([]);
+  
+
+  const handleFileChange = (e) => setFile(e.target.files[0]);
+
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const res = await axios.post('http://localhost:5000/upload', formData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+      console.log(res.data);
+      fetchMetadata(); // Refresh file list
+    } catch (err) {
+      console.error(err.response?.data || err.message);
+    }
+  }
+
+  const fetchMetadata = async () => {
+    const res = await axios.get('http://localhost:5000/files');
+    setMetadata(res.data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>&nbsp;
+      <h1>File Upload</h1>&nbsp;
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
+
+      <h2>Uploaded Files</h2>
+      <ul>
+        {metadata.map(file => (
+          <li key = {file._id}>{file.filename}</li>
+        ))}
+      </ul>
     </div>
+
   );
 }
 
